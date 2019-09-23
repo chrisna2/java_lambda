75a1872adba01c52017bf3f9a2cd2aa5648715cd
KOSTA <KOSTA@DESKTOP-G3Q9FPJ.mshome.net>
2019-09-23 21:29:21 +0900

    람다 : Function

--- src/lambdasinaction/_01lambda/functional/function/FunctionDemo.java
+++ src/lambdasinaction/_01lambda/functional/function/FunctionDemo.java
@@ -25,11 +25,15 @@ public class FunctionDemo {
 
 		
 
-		// 2. lambda expression
-		
+		// 2. lambda expression 
+		// For 문을 쓰지 않는다!
+		List<String> colorList = getColorList(inventory, apple -> apple.getColor());
+		System.out.println(colorList);
 
 		// 3. Method Reference
-		// Function<Apple,String> function = Apple :: getColor;
+		//Function<Apple,String> function = Apple :: getColor;
+		colorList = getColorList(inventory, Apple::getColor);
+		System.out.println(colorList);
 		
 
 	}
