92381f22a78d92ba058de74bfa0160efccb5b4a4
chrisna2 <marksman703@gmail.com>
2019-09-24 21:50:46 +0900

    람다 : anyMatch, allMatch, noneMatch, findAny

--- src/lambdasinaction/_02stream/basic2/_04Finding.java
+++ src/lambdasinaction/_02stream/basic2/_04Finding.java
@@ -19,23 +19,29 @@ public class _04Finding {
 
     //1. anyMatch
     private static boolean isVegetarianFriendlyMenu(){
-        return false;
+    	//적어도 한 개라도 일치 하는 경우 통과
+        return Dish.menu.stream()
+        		.anyMatch(Dish::isVegetarian);
     }
     //2.allMatch
     private static boolean isHealthyMenu(){
-
-        return false;
+    	//모두 조건에 일치하는 경우
+        return Dish.menu.stream()
+        		.allMatch(dish -> dish.getCalories() < 900);
     }
 
     //3. noneMatch
     private static boolean isHealthyMenu2(){
-
-        return false;
+    	//어떤것도 통과 하지 못할 경우
+        return Dish.menu.stream()
+        		.noneMatch(dish -> dish.getCalories() > 900);
     }
     //4. findAny
     private static Optional<Dish> findVegetarianDish(){
-
-        return null;
+    	//현재 스트림의 임의 요소 반환
+        return Dish.menu.stream()
+        		.filter(dish -> dish.getCalories() < 200)
+        		.findAny();
     }
     
 }
