8ba2b7cb06003e6a3ae53805796b8a2f8cc5a36b
chrisna2 <marksman703@gmail.com>
2019-09-26 19:38:49 +0900

    람다 : summerizing, collector 계산

--- src/lambdasinaction/_02stream/collect/_02Summarizing.java
+++ src/lambdasinaction/_02stream/collect/_02Summarizing.java
@@ -6,6 +6,8 @@ import java.util.function.*;
 import static java.util.stream.Collectors.*;
 import static lambdasinaction._02stream.collect.Dish.menu;
 
+import static java.util.Comparator.*;
+
 public class _02Summarizing {
 
     public static void main(String ... args) {
@@ -26,36 +28,43 @@ public class _02Summarizing {
 
     //1. Comparator를 사용한 collect(), reducing()
     private static Dish findMostCaloricDishUsingComparator() {
-
-        return null;
+        return menu.stream()
+        			.collect(maxBy(comparing(Dish::getCalories)))
+        			.get();//Stream<>;
     }
 
     //collect() - reducing 사용
     private static Dish findMostCaloricDish() {
-        return menu.stream().collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)).get();
+        return menu.stream()
+        			.collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2))
+        			.get();
     }
 
     //2.  summingInt() 사용
     private static int calculateTotalCalories() {
-
-        return 0;
+    	//합계구하기
+        return menu.stream().collect(summingInt(dish -> dish.getCalories()));
     }
 
     //3. averagingInt() 사용
     private static Double calculateAverageCalories() {
-
-        return 0.0;
+    	//평균구하기
+        return menu.stream().collect(averagingInt(Dish::getCalories));
     }
 
     //4. summarizingInt() 사용
     private static IntSummaryStatistics calculateMenuStatistics() {
-        return null;
+    	//IntSummaryStatistics{count=9, sum=4300, min=120, average=477.777778, max=800}
+    	//각각의 수치들 계산의 요약본
+        return menu.stream().collect(summarizingInt(Dish::getCalories));
     }
 
     //5. joining() 사용
     private static String getShortMenu() {
-
-        return "";
+    	//구분자를 입력해서 출력
+        return menu.stream()
+        		.map(Dish::getName)
+        		.collect(joining("/"));
     }
 
     private static String getShortMenuCommaSeparated() {
