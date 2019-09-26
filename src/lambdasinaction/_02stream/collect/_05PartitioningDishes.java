8a76f82ba70d66d33f715becf428ef4edfc4f276
chrisna2 <marksman703@gmail.com>
2019-09-26 20:39:50 +0900

    람다 : partitioningBy

--- src/lambdasinaction/_02stream/collect/_05PartitioningDishes.java
+++ src/lambdasinaction/_02stream/collect/_05PartitioningDishes.java
@@ -2,6 +2,7 @@ package lambdasinaction._02stream.collect;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.*;
 import static java.util.Comparator.*;
@@ -15,20 +16,26 @@ public class _05PartitioningDishes {
         System.out.println("Most caloric dishes by vegetarian: " + mostCaloricPartitionedByVegetarian());
     }
 
-    //1. 채식요리와 채식요리가 아닌 것으로 분류하기
+    // 1. 채식요리와 채식요리가 아닌 것으로 분류하기
     private static Map<Boolean, List<Dish>> partitionByVegeterian() {
         return menu.stream()
         		.collect(partitioningBy(Dish::isVegetarian));
     }
-    //2. 채식요리와 채식요리가 아닌 것으로 분류한 다음 type별로 그룹핑 하기
+    // 2. 채식요리와 채식요리가 아닌 것으로 분류한 다음 type별로 그룹핑 하기
     private static Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
-        return null;
+        return menu.stream()
+        		.collect(partitioningBy(Dish::isVegetarian,
+        							    groupingBy(Dish::getType)));
     }
 
-    //3. 채식요리와 채식요리가 아닌 것으로 분류한 다음
-    // 채식요리 중 칼로리 가장 높은 Dish, 채식요리가 아닌 것 중 칼로리 가장 높은 Dish
+    // 3. 채식요리와 채식요리가 아닌 것으로 분류한 다음
+    // 채식요리 중 칼로리 가장 높은 Dish, 채식요리가 아닌 것 중 칼로리 가장 높은 Dish => 파티션은 오직 두개 밖애 구분이 안된다, 조건도 임의로 정해 줘야 한다.
     private static Map<Boolean,Dish> mostCaloricPartitionedByVegetarian() {
-        return null;
+        return menu.stream()
+        		.collect(partitioningBy(Dish::isVegetarian,
+        								collectingAndThen(maxBy(comparing(Dish::getCalories)),
+        											            Optional<Dish>::get)
+        								));
     }
 }
 
